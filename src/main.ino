2a6f2755a49c2b7d010c4832c27a095337d99129
Benjamin Paquet <tiben_96@hotmail.ca>
2019-10-17 14:01:52 -0400

    a

--- src/main.ino
+++ src/main.ino
@@ -7,8 +7,12 @@ Date: Derniere date de modification
 */
 
 #include <LibRobus.h> // Essentielle pour utiliser RobUS
+<<<<<<< Updated upstream
 #include<avancer/avancer.h>
 #include<tourner/tourner.h>
+=======
+#include <parcours/parcoursRobotA.h>
+>>>>>>> Stashed changes
 
 /* ****************************************************************************
 Fonctions d'initialisation (setup)
@@ -19,6 +23,7 @@ Fonctions d'initialisation (setup)
 
 #define robot3A 0
 #define robot3B 1
+<<<<<<< Updated upstream
 #define vitesse 0.25
 #define delais 500
 
@@ -52,6 +57,110 @@ void setup()
   // tourner(1,30,0.25); // 5
   // delay(500);
   // Avancer(110, 0.25, robot3B);
+=======
+#define vitesseAvancer 0.25
+#define vitesseTourner 0.25
+#define delais 200
+
+void parcourTest(int noRobot)
+{
+  // Parcour planché
+  Avancer(120.5, vitesseAvancer, noRobot);
+  delay(delais);
+
+  tourner(LEFT, 90, vitesseTourner, noRobot); //1
+  delay(delais);
+
+  Avancer(78, vitesseAvancer, noRobot);
+  delay(delais);
+
+  tourner(RIGHT, 90, vitesseTourner, noRobot); // 2
+  delay(delais);
+
+  Avancer(103.5, vitesseAvancer, noRobot);
+  delay(delais);
+
+  tourner(RIGHT, 30, vitesseTourner, noRobot); // 3
+  delay(delais);
+
+  Avancer(170, vitesseAvancer, noRobot);
+  delay(delais);
+
+  tourner(LEFT, 90, vitesseTourner, noRobot); // 4
+  delay(delais);
+
+  Avancer(45, vitesseAvancer, noRobot);
+  delay(delais);
+
+  tourner(RIGHT, 30, vitesseTourner, noRobot); // 5
+  delay(delais);
+
+  Avancer(125, vitesseAvancer, noRobot);
+}
+
+void defiParcourt(int noRobot)
+{
+  Avancer(50, vitesseAvancer, noRobot);
+  delay(delais);
+
+  tourner(LEFT, 87.568989, vitesseTourner, noRobot); //1
+  delay(delais);
+
+  Avancer(50, vitesseAvancer, noRobot);
+  delay(delais);
+
+  tourner(RIGHT, 88, vitesseTourner, noRobot); //2
+  delay(delais);
+
+  Avancer(45, vitesseAvancer, noRobot);
+  delay(delais);
+
+  tourner(RIGHT, 88, vitesseTourner, noRobot); //3
+  delay(delais);
+
+  Avancer(55, vitesseAvancer, noRobot);
+  delay(delais);
+
+  tourner(LEFT, 88, vitesseTourner, noRobot); //4
+  delay(delais);
+
+  Avancer(100, vitesseAvancer, noRobot);
+  delay(delais);
+
+  tourner(RIGHT, 91, vitesseTourner, noRobot); //5
+  delay(delais);
+
+  Avancer(46, vitesseAvancer, noRobot);
+  delay(delais);
+
+  tourner(LEFT, 88.5, vitesseTourner, noRobot); //6
+  delay(delais);
+
+  Avancer(125, vitesseAvancer, noRobot);
+  delay(delais);
+}
+
+void choixRobot(int noRobot)
+{
+  if (noRobot == robot3A)
+    defiParcoursA(robot3A, vitesseAvancer, vitesseTourner, delais); //RobotA
+  else
+    defiParcoursB(robot3B, vitesseAvancer, vitesseTourner, delais); //RobotB
+}
+
+void setup()
+{
+  BoardInit();
+  //Activer les fonctions quand le bunper arrière est cliqué.
+  if(ROBUS_IsBumper(3))
+    choixRobot(robot3B);
+
+  // defiParcourt(robot3A);
+  // defiParcourt(robot3B);
+  // defiParcoursA(robot3A, vitesseAvancer, vitesseTourner, delais); //RobotA
+  // defiParcoursB(robot3A, vitesseAvancer, vitesseTourner, delais); //RobotB
+  // tourner(LEFT,90, vitesseTourner,robot3A);
+>>>>>>> Stashed changes
 }
 
 /* ****************************************************************************
--- src/tourner/tourner.h
+++ src/tourner/tourner.h
@@ -1,12 +1,26 @@
 #include<LibRobus.h>
 #define constPond 0.00001
 
+<<<<<<< Updated upstream
 void tourner(bool direction, float angle, float vitesse, int noRobot){
 
     float tourComplet = 30;
     float distance = (tourComplet/360) * angle;
 
     encodeur(noRobot, "tourner", vitesse, constPond, distance, direction);
+=======
+void tourner(bool direction, float angle, float vitesse, int noRobot)
+{
+    float tourComplet = 26.7;
+
+    //robotB
+    if(noRobot == 1){
+        tourComplet = 28.5;
+    }
+    if(noRobot == 0){
+        tourComplet = 28.5;
+    }
+>>>>>>> Stashed changes
 
     // float Distance_parcourue = 0;
     // float Encodeur_Gauche = 0;
